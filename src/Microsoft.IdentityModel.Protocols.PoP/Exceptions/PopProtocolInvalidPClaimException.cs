d9cd5123ef29864265a53c407cc3858115112a51
George Krechar <dokrecar@microsoft.com>
2019-08-23 16:11:59 -0700

    Add more granular exceptions

--- src/Microsoft.IdentityModel.Protocols.PoP/Exceptions/PopProtocolInvalidPClaimException.cs
+++ src/Microsoft.IdentityModel.Protocols.PoP/Exceptions/PopProtocolInvalidPClaimException.cs
@@ -30,32 +30,31 @@ using System;
 namespace Microsoft.IdentityModel.Protocols.PoP
 {
     /// <summary>
-    /// This exception is thrown when an Pop protocol handler encounters a protocol error.
     /// </summary>
-    public class PopProtocolException : Exception
+    public class PopProtocolInvalidPClaimException : PopProtocolValidationException
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="PopProtocolException"/> class.
+        /// Initializes a new instance of the <see cref="PopProtocolInvalidPClaimException"/> class.
         /// </summary>
-        public PopProtocolException()
+        public PopProtocolInvalidPClaimException()
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PopProtocolException"/> class.
+        /// Initializes a new instance of the <see cref="PopProtocolInvalidPClaimException"/> class.
         /// </summary>
         /// <param name="message">Additional information to be included in the exception and displayed to user.</param>
-        public PopProtocolException(string message)
+        public PopProtocolInvalidPClaimException(string message)
             : base(message)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PopProtocolException"/> class.
+        /// Initializes a new instance of the <see cref="PopProtocolInvalidPClaimException"/> class.
         /// </summary>
         /// <param name="message">Additional information to be included in the exception and displayed to user.</param>
         /// <param name="innerException">A <see cref="Exception"/> that represents the root cause of the exception.</param>
-        public PopProtocolException(string message, Exception innerException)
+        public PopProtocolInvalidPClaimException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
--- src/Microsoft.IdentityModel.Protocols.PoP/Exceptions/PopProtocolValidationException.cs
+++ src/Microsoft.IdentityModel.Protocols.PoP/Exceptions/PopProtocolValidationException.cs
@@ -30,32 +30,31 @@ using System;
 namespace Microsoft.IdentityModel.Protocols.PoP
 {
     /// <summary>
-    /// This exception is thrown when an Pop protocol handler encounters a protocol error.
     /// </summary>
-    public class PopProtocolException : Exception
+    public class PopProtocolValidationException : PopProtocolException
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="PopProtocolException"/> class.
+        /// Initializes a new instance of the <see cref="PopProtocolValidationException"/> class.
         /// </summary>
-        public PopProtocolException()
+        public PopProtocolValidationException()
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PopProtocolException"/> class.
+        /// Initializes a new instance of the <see cref="PopProtocolValidationException"/> class.
         /// </summary>
         /// <param name="message">Additional information to be included in the exception and displayed to user.</param>
-        public PopProtocolException(string message)
+        public PopProtocolValidationException(string message)
             : base(message)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PopProtocolException"/> class.
+        /// Initializes a new instance of the <see cref="PopProtocolValidationException"/> class.
         /// </summary>
         /// <param name="message">Additional information to be included in the exception and displayed to user.</param>
         /// <param name="innerException">A <see cref="Exception"/> that represents the root cause of the exception.</param>
-        public PopProtocolException(string message, Exception innerException)
+        public PopProtocolValidationException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
